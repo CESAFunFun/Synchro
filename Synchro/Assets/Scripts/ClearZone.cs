c879f7161692dfa014ec9aaf81dda79745229c92
Irfan Fahmi Ramadhan <lamperouge2702@gmail.com>
2017-05-19 00:53:00 +0900

    ClearとDeathZoneを追加

---
+++ Synchro/Assets/Scripts/ClearZone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearZone : MonoBehaviour
+{
+    public bool isClear = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" || other.tag == "Child")
+        {
+            isClear = true;
+        }
+    }
+}
---
+++ Synchro/Assets/Scripts/DeathZone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+    public bool isFail = false; 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player" || other.tag == "Child")
+        {
+            isFail = true;
+        }
+    }
+}
