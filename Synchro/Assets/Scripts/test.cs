f97e1737a3ab2affa0e0a2437f59d0ae43c11c01
kkkkkkkkkkkkkkk <fkei120@yahoo.co.jp>
2017-05-25 09:33:09 +0900

    マップ読み込みCSの更新

--- Synchro/Assets/Scripts/test.cs
+++ Synchro/Assets/Scripts/test.cs
@@ -2,45 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using System;
 
 public class test : MonoBehaviour {
 
     [SerializeField]
-    private TextAsset a;
+    private TextAsset map;
+
+    [SerializeField]
+    private GameObject[] stageObject;
+
 
     // Use this for initialization
     void Start()
     {
         int x = 0;
         int y = 0;
-        StringReader reader = new StringReader(a.text);
+        StringReader reader = new StringReader(map.text);
         while (reader.Peek() > -1)
         {
             string line = reader.ReadLine();
             string[] values = line.Split(',');
-            GameObject obj;
+            GameObject obj = null;
             foreach (string str in values)
             {
-                print(str);
-                switch (str)
-                {
-                    case "0":
-                        obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                        break;
-                    case "2":
-                        obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                        break;
-                    case "3":
-                        obj = GameObject.CreatePrimitive(PrimitiveType.Capsule);
-                        break;
-                    default:
-                        obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                        break;
-                }
 
-                if (obj != null) obj.transform.position = new Vector3(x, y, 0);
-                if (obj != null) obj.transform.localScale = new Vector3(5, 5, 5);
-                obj.transform.SetParent(transform);
+                int a = int.Parse(str);
+                print(a);
+                if (a >= 0 && a < stageObject.Length)
+                {
+                    obj = Instantiate(stageObject[a], transform);
+                    obj.transform.position = new Vector3(x, y, 0);
+                    obj.transform.localScale = new Vector3(5, 5, 5);
+                }
                 x += 5;
             }
             x = 0;
