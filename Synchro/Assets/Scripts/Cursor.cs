25d4ab7e3f21a0dcdf56c0b76c609b7cd49fac41
TakayoshiOgawa <ogawa4283gamecreator@gmail.com>
2017-05-31 01:56:55 +0900

    UIを用いたセレクトシーンの実装

---
+++ Synchro/Assets/Scripts/Cursor.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cursor : MonoBehaviour
+{
+
+    [SerializeField]
+    private Gamepad _gamepad;
+
+    [SerializeField]
+    private int number = 8;
+
+    [SerializeField]
+    private string nextSceneName;
+
+    [SerializeField]
+    private Vector2 _lerpLength = Vector2.one;
+
+    private RectTransform _rectTransform;
+    private bool _isLerp;
+    private float _startTime;
+    private Vector3 _startPosition;
+    private Vector3 _endPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+        _isLerp = false;
+        _startPosition = transform.position;
+        _endPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _rectTransform.Rotate(Vector3.forward * 3F);
+
+        if (_isLerp)
+        {
+            var diff = Time.timeSinceLevelLoad - _startTime;
+            if (diff > 1F)
+            {
+                _rectTransform.position = _endPosition;
+                _isLerp = false;
+            }
+
+            _rectTransform.position = transform.position = Vector3.Lerp(_startPosition, _endPosition, diff / 1F);
+        }
+        else
+        {
+            _startTime = Time.timeSinceLevelLoad;
+            _startPosition = _rectTransform.position;
+
+            if (_gamepad.leftStick.x <= -1F)
+            {
+                _endPosition += Vector3.left * _lerpLength.x;
+                number += -1;
+                _isLerp = true;
+            }
+            if (_gamepad.leftStick.x >= 1F)
+            {
+                _endPosition += Vector3.right * _lerpLength.x;
+                number += +1;
+                _isLerp = true;
+            }
+
+            if (_gamepad.leftStick.y <= -1F)
+            {
+                _endPosition += Vector3.down * _lerpLength.y;
+                number += +5;
+                _isLerp = true;
+            }
+            if (_gamepad.leftStick.y >= 1F)
+            {
+                _endPosition += Vector3.up * _lerpLength.y;
+                number += -5;
+                _isLerp = true;
+            }
+
+            if (_gamepad.buttonX.trigger)
+            {
+                GameManager.Instance.mapLevel = number;
+                SceneManager.LoadScene(nextSceneName);
+            }
+        }
+    }
+}
