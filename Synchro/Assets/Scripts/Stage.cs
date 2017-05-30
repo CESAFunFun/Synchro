e832039c60e9ec27362b4ff17014a8b0c3bc2a5e
TakayoshiOgawa <ogawa4283gamecreator@gmail.com>
2017-05-31 00:07:13 +0900

    マップチップスクリプトのリファクタリングとUnitySceneの整理

--- Synchro/Assets/Scripts/Stage.cs
+++ Synchro/Assets/Scripts/Stage.cs
@@ -6,14 +6,4 @@ public class Stage : MonoBehaviour {
 
     public int stageLevel;
     public GameObject[] stageObjects;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
