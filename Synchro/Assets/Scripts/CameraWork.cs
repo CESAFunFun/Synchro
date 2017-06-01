cba5ef3a98937215fc90e187fb3dc024068e79c3
kkkkkkkkkkkkkkk <fkei120@yahoo.co.jp>
2017-06-01 09:36:51 +0900

    カメラワーク修正

--- Synchro/Assets/Scripts/CameraWork.cs
+++ Synchro/Assets/Scripts/CameraWork.cs
@@ -14,13 +14,15 @@ public class CameraWork : MonoBehaviour {
     private Camera _subCamera;
 
     private float _angle;
-    private int _side;
+    private Vector2 _side;
+    private Vector2 _sideMax;
 
 	// Use this for initialization
 	void Start () {
         _angle = 0F;
-        _side = 0;
-	}
+        _side = Vector2.zero;
+        _sideMax = Vector2.zero;
+    }
 
     // Update is called once per frame
     void Update() {
@@ -28,8 +30,9 @@ public class CameraWork : MonoBehaviour {
         {
             // アクティブとなっていない方のカメラに切り替える
             ChangeDisplay(!_mainCamera.gameObject.activeSelf);
+            _sideMax = Vector2.zero;
         }
-
+        
         // カメラの角度を変える
         RotationCamera();
     }
@@ -48,33 +51,65 @@ public class CameraWork : MonoBehaviour {
         //回転している最中かの真偽が返ってくる
         // 左への回転処理
         if (RotaTarget()) return;
-        
+
 
         //入力処理
-        //回転してなかったら処理
+        //回転してなかったら処理(X軸)
         if (_gamepad.rightStick.x <= -1F)
         {
-            _side = +1;
+            if (_sideMax.x < 1)
+            {
+                _side.x = +1;
+                _sideMax.x += 1;
+            }
         }
         if (_gamepad.rightStick.x >= 1F)
         {
-            _side = -1;
+            if (_sideMax.x > -1)
+            {
+                _side.x = -1;
+                _sideMax.x -= 1;
+            }
         }
+        ////回転してなかったら処理(Y軸)
+        //if (_gamepad.rightStick.y <= -1F)
+        //{
+        //    if (_sideMax.y < 1)
+        //    {
+        //        _side.y = +1;
+        //        _sideMax.y += 1;
+        //    }
+        //}
+        //if (_gamepad.rightStick.y >= 1F)
+        //{
+        //    if (_sideMax.y > -1)
+        //    {
+        //        _side.y = -1;
+        //        _sideMax.y -= 1;
+        //    }
+        //}
     }
 
     bool RotaTarget()
     {
         // 回転中かどうかを判定
-        if (_side != 0 && _angle < 45F)
+        if (_side.x != 0 && _angle < 45F)
         {
             _angle += Time.deltaTime * _roteSpeed;
-            transform.Rotate((Vector3.up * _side) * Time.deltaTime * _roteSpeed);
+            transform.Rotate((Vector3.up * _side.x) * Time.deltaTime * _roteSpeed);
             return true;
         }
+        //// 回転中かどうかを判定
+        //if (_side.y != 0 && _angle < 45F)
+        //{
+        //    _angle += Time.deltaTime * _roteSpeed;
+        //    transform.Rotate((Vector3.right * _side.y) * Time.deltaTime * _roteSpeed);
+        //    return true;
+        //}
 
         // 回転していない状態に戻す
         _angle = 0F;
-        _side = 0;
+        _side = Vector2.zero;
         return false;
     }
 }
