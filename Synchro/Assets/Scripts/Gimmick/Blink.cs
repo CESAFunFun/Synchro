0cb6d87bb099c7965c9e40544d5d22b164ab296b
Irfan Fahmi Ramadhan <lamperouge2702@gmail.com>
2017-06-01 12:09:54 +0900

    ギミックの追加

---
+++ Synchro/Assets/Scripts/Gimmick/Blink.cs
@@ -0,0 +1,54 @@
+﻿//-----------------------
+// ブリングするギミック
+//-----------------------
+using UnityEngine;
+
+public class Blink : MonoBehaviour {
+
+    [SerializeField]
+    private float _interval = 2;
+
+    private float _time = 0;
+
+    private bool _enabled = true;
+
+    private SpriteRenderer _spriteRend;
+    private BoxCollider _boxCol;
+
+	// Use this for initialization
+	void Start () {
+
+        _spriteRend = gameObject.GetComponent<SpriteRenderer>();
+        _boxCol = gameObject.GetComponent<BoxCollider>();
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _time++;
+        
+        //時間たつと消したり出したりする
+        if(_time >= 60 * _interval)
+        {
+            _time = 0;
+            if(_enabled)
+            {
+                _enabled = false;
+            }
+            else
+            {
+                _enabled = true;
+            }
+        }
+
+        EnablingDisabling(_enabled);
+
+	}
+
+    private void EnablingDisabling(bool flag)
+    {
+        _spriteRend.enabled = flag;
+        _boxCol.enabled = flag;
+    }
+}
---
+++ Synchro/Assets/Scripts/Gimmick/MovingFloor.cs
@@ -0,0 +1,36 @@
+﻿//---------------------------------
+// 横に進んだり戻ったりするギミック
+//---------------------------------
+using UnityEngine;
+
+public class MovingFloor : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 1;
+
+    private float posX;
+	// Use this for initialization
+	void Start () {
+
+        posX = gameObject.transform.position.x;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        posX += speed;
+
+        gameObject.transform.position = new Vector3(posX, transform.position.y, transform.position.z);
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {        
+        if(collision.gameObject.tag == "Map")
+        {
+            speed *= -1;
+        }
+    }
+
+}
