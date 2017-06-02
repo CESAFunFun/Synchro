463b0ae97d03b2fe1b8bb5d04ad7bf5c4bbf8599
kkkkkkkkkkkkkkk <fkei120@yahoo.co.jp>
2017-06-02 11:52:13 +0900

    カメラ移動

--- Synchro/Assets/Scripts/CameraMove.cs
+++ Synchro/Assets/Scripts/CameraMove.cs
@@ -9,6 +9,11 @@ public class CameraMove : MonoBehaviour {
     [SerializeField]
     private float speed=5;
 
+    [SerializeField]
+    private Camera _mainCamera;
+    [SerializeField]
+    private Camera _subCamera;
+
     // Use this for initialization
     void Start () {
 
@@ -21,6 +26,11 @@ public class CameraMove : MonoBehaviour {
         if(_gamepad==null)
             _gamepad = _gamepad = GameManager.Instance.gamePad;
 
+        if (_gamepad.rightStickPress.trigger)
+        {
+            // アクティブとなっていない方のカメラに切り替える
+            ChangeDisplay(!_mainCamera.gameObject.activeSelf);
+        }
 
         Vector3 move = Vector3.zero;
         //入力処理
@@ -30,7 +40,10 @@ public class CameraMove : MonoBehaviour {
             //左端までしか移動できないようにする
             if (transform.position.x > 0)
             {
-                move.x = -speed;
+                if (_mainCamera.gameObject.active)
+                    move.x = -speed;
+                else
+                    move.x = speed;
                 transform.Translate(move * Time.deltaTime);
             }
         }
@@ -38,7 +51,10 @@ public class CameraMove : MonoBehaviour {
         {
             if (transform.position.x < 100)
             {
-                move.x = speed;
+                if (_mainCamera.gameObject.active)
+                    move.x = speed;
+                else
+                    move.x = -speed;
                 transform.Translate(move * Time.deltaTime);
             }
         }
@@ -62,5 +78,13 @@ public class CameraMove : MonoBehaviour {
             }
         }
 
+    }
+    private void ChangeDisplay(bool main)
+    {
+        // どちらかのアクティブが有効になる
+        _mainCamera.gameObject.SetActive(main);
+        _subCamera.gameObject.SetActive(!main);
+        // ここで正面に角度を修正させる
+        transform.rotation = Quaternion.identity;
     }
 }
