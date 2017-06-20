e356156a0c201a92345081e71bf8a2d9a4e0487b
Irfan Fahmi Ramadhan <lamperouge2702@gmail.com>
2017-06-20 23:52:56 +0900

    クリアパーティクルを追加

--- Synchro/Assets/Scripts/ClearZone.cs
+++ Synchro/Assets/Scripts/ClearZone.cs
@@ -5,11 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class ClearZone : MonoBehaviour
 {
+    private ParticleSystem _clearPar;
+
+    private void Start()
+    {
+        _clearPar = transform.GetChild(0).GetComponent<ParticleSystem>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Child")
         {
             GameObject.Find("GameManager").SendMessage("Clear");
+            _clearPar.Play();
         }
     }
 }
